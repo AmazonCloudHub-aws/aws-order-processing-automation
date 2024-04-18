4534e9cd01c7eef818969cfd446585f07cdb09c6
dhamsey3 <73405591+dhamsey3@users.noreply.github.com>
2024-04-18 09:27:11 +0200

    Enable S3 Bucket Encryption
    ensuring that data at rest is encrypted

--- main.tf
+++ main.tf
@@ -1,11 +1,18 @@
 # Create an S3 bucket
 resource "aws_s3_bucket" "top_secret_data" {
-  bucket = "top-secret-data" 
+  bucket = "top-secret-data"
   acl    = "private"
 
-  # Enable versioning for the bucket
   versioning {
     enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
   }
 }
 
